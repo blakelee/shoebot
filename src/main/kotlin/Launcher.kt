365a710b4f593f891bd9c0c8127da86ca0b7b674
Blake Oliveira <blake@blakelee.net>
2017-01-22 18:57:18 -0800

    Initial commit with tornadofx demo (Crud.kt)

---
+++ src/main/kotlin/Launcher.kt
@@ -0,0 +1,58 @@
+import cells.Cells
+import circledrawer.CircleDrawer
+import counter.Counter
+import crud.Crud
+import flightbooker.FlightBooker
+import javafx.geometry.Insets
+import javafx.geometry.Pos
+import javafx.scene.Scene
+import javafx.stage.Stage
+
+import temperature.TemperatureConverter
+import timer.Timer
+import tornadofx.*
+import kotlin.reflect.KClass
+import kotlin.reflect.primaryConstructor
+
+class Launcher : App(Solutions::class)
+
+class Solutions : View("7 GUIs Launcher") {
+
+    val openStages = arrayListOf<Stage>()
+
+    override val root = vbox {
+        padding = Insets(10.0)
+        spacing = 10.0
+        alignment = Pos.CENTER
+
+        val map = mapOf("Counter" to Counter::class,
+                "Temperature Converter" to TemperatureConverter::class,
+                "Flight Booker" to FlightBooker::class,
+                "Timer" to Timer::class,
+                "CRUD" to Crud::class,
+                "Circle Drawer" to CircleDrawer::class,
+                "Cells" to Cells::class)
+
+        for ((name, kClass) in map.entries) {
+            button(name) {
+                prefWidth = 200.0
+                setOnAction {
+                    val view = kClass.primaryConstructor?.call()
+                    val stage = Stage()
+                    stage.scene = Scene(view?.root)
+                    stage.title = name
+                    stage.show()
+                    openStages.add(stage)
+                }
+            }
+        }
+    }
+
+    init {
+        primaryStage.setOnCloseRequest {
+            for (s in openStages) {
+                s.close()
+            }
+        }
+    }
+}
